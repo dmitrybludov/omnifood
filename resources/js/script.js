346b57f9127b2c192ff942e63f88a6a6ecb4e682
Dmitry Bludov <bludovdmitry@gmail.com>
2019-09-13 23:00:44 +0300

    compressed images

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+	/* For the sticky navigation */
 	$('.js--section-features').waypoint(function(direction) {
 		if (direction == 'down') {
 			$('nav').addClass('sticky');
@@ -9,5 +9,14 @@ $(document).ready(function() {
 	}, {
 		offset: '60px'
 	});
+
+	/* Scroll on buttons */
+	$('.js--scroll-to-plans').click(function() {
+		$('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+	});
+
+	$('.js--scroll-to-start').click(function() {
+		$('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+	});    
 
 });
