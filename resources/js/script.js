65f9f9233bc5f9b846f0b4b9e6d72e1ac37e10b7
Dmitry Bludov <bludovdmitry@gmail.com>
2019-09-13 23:22:57 +0300

    added animation effects

--- resources/js/script.js
+++ resources/js/script.js
@@ -17,6 +17,31 @@ $(document).ready(function() {
 
 	$('.js--scroll-to-start').click(function() {
 		$('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
-	});    
+	});
+
+	/* Animations on scroll */
+	$('.js--wp-1').waypoint(function(direction) {
+		$('.js--wp-1').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+
+	$('.js--wp-2').waypoint(function(direction) {
+		$('.js--wp-2').addClass('animated fadeInUp');
+	}, {
+		offset: '50%'
+	});
+
+	$('.js--wp-3').waypoint(function(direction) {
+		$('.js--wp-3').addClass('animated fadeInUp');
+	}, {
+		offset: '50%'
+	});
+
+	$('.js--wp-4').waypoint(function(direction) {
+		$('.js--wp-4').addClass('animated pulse');
+	}, {
+		offset: '50%'
+	});
 
 });
